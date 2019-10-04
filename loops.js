549211590921497318c69985c9aabb4910c6e008
Joseph Moon <nakkta@gmail.com>
2019-10-04 21:14:02 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(array){
   for (i = 0; i <25, i++)
-    if(i === 0) {
+    if(i === 1) {
       array.push('I am 1 strange loop')
     } else {
       array.push (`I am ${i} strange loops.`)
