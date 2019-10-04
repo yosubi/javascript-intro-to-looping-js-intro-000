d65f9cf766787542fac5cb0e61cb709d50fc04d6
Joseph Moon <nakkta@gmail.com>
2019-10-04 21:13:05 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,4 +1,9 @@
-
-for (var i = 0; i < 25; i++) {
-console.log (`I am ${i} strange loop.`)
+function forLoop(array){
+  for (i = 0; i <25, i++)
+    if(i === 1) {
+      array.push('I am 1 strange loop')
+    } else {
+      array.push (`I am ${i} strange loops.`)
+    }
+    return array;
 }
